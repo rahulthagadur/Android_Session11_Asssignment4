29323f540acc194d55b4cb7f8d0d3b39e3622efd
rahulthagadur <rt_rahul@hotmail.com>
2017-10-13 22:19:18 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/thagadur/android_session11_asssignment4/TableData.java
@@ -0,0 +1,26 @@
+package com.example.thagadur.android_session11_asssignment4;
+
+/**
+ * Created by thagadur on 13/10/17.
+ */
+
+import android.provider.BaseColumns;
+
+
+public class TableData {
+
+    public TableData()
+    {
+
+    }
+
+    public static abstract class TableInfo implements BaseColumns
+    {
+        /** KEYSSSS  */
+        public static final String id = "idd";
+        public static final String firstname = "firstname";
+        public static final String lastname = "lastname";
+        public static final String DATABASE_NAME = "user_info1";
+        public static final String TABLE_NAME = "reg_info1";
+    }
+}
